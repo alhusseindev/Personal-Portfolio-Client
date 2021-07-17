506e367b45498f4f699903bfd1d43ad48151b25a
alhusseindev <alhussein.tarek97@gmail.com>
2021-07-17 16:46:02 +0200

    Personal-Portfolio-Client

--- src/Curtain/Curtain.js
+++ src/Curtain/Curtain.js
@@ -10,7 +10,6 @@ function Curtain(){
                 <ul className="menu__items">
                     <li><a href="http://github.com/alhusseindev" target="_blank" rel="noreferrer" className="nav-links">Projects</a></li>
                     <li><a href="https://drive.google.com/file/d/1xPR4ghXwoYDYXGjuiPFJd0aIDvI60JqK/view?usp=sharing" target="_blank" rel="noreferrer" className="nav-links">Resume</a></li>
-                    {/** There is a problem here */}
                     <li><a href="#contact-inquiry-section" className="nav-links">Contact</a></li>
                 </ul>
             </div>
--- src/NavBar/NavBar.js
+++ src/NavBar/NavBar.js
@@ -8,16 +8,24 @@ import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
 
 function NavBar(){
 
-    const [show, setShow] = useState(false);
+    //const [show, setShow] = useState(false);
+
+    /** const showCurtain = () => {
+        setShow(!show);
+    } */
 
 
     return(
         <nav className="nav__bar">
             <div className="logo">Alhussein</div>
-            <div className="menu__icon" onClick={() => {setShow(!show)} }>
-                { show ? <React.Fragment> <FontAwesomeIcon icon={faTimes} color="floralwhite" size="sm" /> <Curtain/> </React.Fragment> : <FontAwesomeIcon icon={faBars} color="floralwhite" size="sm" /> }
+            <div className="menu__icon">
+                {/** { show ? <FontAwesomeIcon icon={faTimes} color="floralwhite" size="lg" /> : <FontAwesomeIcon icon={faBars} color="floralwhite" size="lg" /> } */}
+                <ul className="header-list">
+                    <li className="header-points"><a className="header__links" href="https://drive.google.com/file/d/1xPR4ghXwoYDYXGjuiPFJd0aIDvI60JqK/view?usp=sharing" target="_blank" rel="noreferrer">Resume</a></li>
+                    <li className="header-points"><a className="header__links" href="http://github.com/alhusseindev" target="_blank" rel="noreferrer">Projects</a></li>
+                    <li className="header-points"><a className="header__links" href="#contact-inquiry-section">Contact Me</a></li>
+                </ul>
             </div>
-
         </nav>
     );
 
